a9f54e53828dba86a09e3a40bd236ca8addb0a61
jacobcalv <jacobcalvino@outlook.com>
2019-09-10 19:37:55 -0500

    able to console.log my inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,45 @@ import React, {useState} from 'react';
 
 
 const Form = () => {
-    const [newEntry, setNewEntry]  = useState()
-    const handleChange = () => {};
+    const [newTeamMember, setNewTeamMember]  = useState({
+        name: '',
+        email: '',
+        role: ''
+    })
+    
+    const handleChange = (event) => {
+        setNewTeamMember({ ...newTeamMember, [event.target.name]: event.target.value})
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(newTeamMember)
+    }
+
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
+            <label>Name: </label>
             <input 
+                name='name'
                 type='text'
-                placeholder='Enter Name Here'/>
+                onChange={handleChange}
+                placeholder='Enter Name Here'
+            />
+            <label>Email: </label>
+            <input 
+                name='email'
+                type='text'
+                onChange={handleChange}
+                placeholder='Enter Email Here'
+            />
+            <label>Role: </label>
+            <input 
+                name='role'
+                type='text'
+                onChange={handleChange}
+                placeholder='Enter Role Here'
+            />
             <button type='submit'>Submit</button>
         </form>
     )
--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
+import styled from 'styled-components'
 
 const TeamCard = (props) => {
+    const Card = styled.div`
+        width: 40%;
+        display: flex;
+        flex-direction: column;
+        border: 2px solid black;
+        border-radius: 5px;
+        margin-top: 3rem;
+        margin-bottom: 3rem;
+        margin-left: 30%;
+    `
+    const Name = styled.h2`
+        text-align: center;
+    `
+    const Email = styled.p`
+        text-align: center;
+    `
+    const Role = styled.p`
+        text-align: center;
+    `
      return (
          <>
             {props.teamMember.map((member,index) =>(
-                <div className='card' key={index}>
-                    <h2>{member.name}</h2>
-                    <p>{member.email}</p>
-                    <p>{member.role}</p>
-                </div>
+                <Card key={index}>
+                    <Name>Name: {member.name}</Name>
+                    <Email>Email: {member.email}</Email>
+                    <Role>Role: {member.role}</Role>
+                </Card>
             ))}
          </>
      )
